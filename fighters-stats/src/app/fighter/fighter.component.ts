b3a0dde197c5fc69c2da5a765b067b6aa23dffbe
Borloo <supermaxime753@gmail.com>
2023-10-25 15:25:02 +0200

    Service -> component -> vue

--- fighters-stats/src/app/fighter/fighter.component.ts
+++ fighters-stats/src/app/fighter/fighter.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Fighter} from "./fighter.model";
+import {FighterService} from "./fighter.service";
 
 @Component({
   selector: 'app-fighter',
   templateUrl: './fighter.component.html',
   styleUrls: ['./fighter.component.scss']
 })
-export class FighterComponent {
+export class FighterComponent implements OnInit{
+
+  fightersList: Array<Fighter>|undefined
+
+  constructor(private fighterService: FighterService) {
+  }
+
+  ngOnInit() {
+    this.fighterService.getAllFighters().subscribe(resFighters => {
+      this.fightersList = resFighters
+    })
+  }
 
 }
