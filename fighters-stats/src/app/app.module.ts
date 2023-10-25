3295b3fa736702c3c599733328d091509ba29a82
Borloo <supermaxime753@gmail.com>
2023-10-25 13:28:54 +0200

    ng g c fighter

--- fighters-stats/src/app/app.module.ts
+++ fighters-stats/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FighterComponent } from './fighter/fighter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FighterComponent
   ],
   imports: [
     BrowserModule,
