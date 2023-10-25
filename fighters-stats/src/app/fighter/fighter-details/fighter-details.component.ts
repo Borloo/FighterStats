ad9e89e9d912aac4047ca27a1b1c3e428a34227d
JulienOusmer <julienousmer46090@gmail.com>
2023-10-25 16:25:49 +0200

    auto remplissage form

--- fighters-stats/src/app/fighter/fighter-details/fighter-details.component.ts
+++ fighters-stats/src/app/fighter/fighter-details/fighter-details.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {FormBuilder} from "@angular/forms";
 import {Fighter} from "../fighter.model";
+import {Category} from "../../category/category.model";
 
 @Component({
   selector: 'app-fighter-details',
@@ -10,22 +11,33 @@ import {Fighter} from "../fighter.model";
 export class FighterDetailsComponent {
 
   @Input()
-  model: Fighter | null = null;
+  model: Fighter;
   @Output()
   emitFighter: EventEmitter<Fighter> = new EventEmitter<Fighter>();
 
   fighterForm = this.fb.group({
     firstName: [''],
     name: [''],
-    age: [''],
-    weight: [''],
-    height: [''],
-    reach: [''],
-    nbWin: [''],
-    nbLose: [''],
+    age: [0],
+    weight: [0],
+    height: [0],
+    reach: [0],
+    nbWin: [0],
+    nbLose: [0],
     sexe: [''],
-    category: ['']
+    category: [null]
   })
+
+  ngOnInit() {
+
+    if (this.model === null) {
+      this.model = new Fighter(1, '', '', 0, 0, 0, 0, 0, 0, '', null);
+    } else {
+      // @ts-ignore
+      this.fighterForm.patchValue(this.model);
+    }
+  }
   constructor(private fb: FormBuilder) {
+    this.model = new Fighter(1, '', '', 0, 0, 0, 0, 0, 0, '', null);
   }
 }
