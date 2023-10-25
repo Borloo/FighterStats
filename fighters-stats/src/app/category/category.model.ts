e2568ceb58215ee309bc0527328f1ce30d71634c
JulienOusmer <julienousmer46090@gmail.com>
2023-10-25 14:12:57 +0200

    Add category service

--- fighters-stats/src/app/category/category.model.ts
+++ fighters-stats/src/app/category/category.model.ts
@@ -1,14 +1,14 @@
 import {Fighter} from '../fighter/fighter.model';
 
 export class Category {
-  private _id: number;
-  private _name: string;
-  private _min_weight: number;
-  private _max_weight: number;
-  private _champion: Fighter;
+  _id: number;
+  _name: string;
+  _min_weight: number;
+  _max_weight: number;
+  _champion: Fighter | null;
 
   constructor(id: number, name: string, min_weight: number,
-              max_weight: number, champion: Fighter) {
+              max_weight: number, champion: Fighter | null = null) {
     this._id = id;
     this._name = name;
     this._min_weight = min_weight;
@@ -16,44 +16,4 @@ export class Category {
     this._champion = champion;
   }
 
-
-  get id(): number {
-    return this._id;
-  }
-
-  set id(value: number) {
-    this._id = value;
-  }
-
-  get name(): string {
-    return this._name;
-  }
-
-  set name(value: string) {
-    this._name = value;
-  }
-
-  get min_weight(): number {
-    return this._min_weight;
-  }
-
-  set min_weight(value: number) {
-    this._min_weight = value;
-  }
-
-  get max_weight(): number {
-    return this._max_weight;
-  }
-
-  set max_weight(value: number) {
-    this._max_weight = value;
-  }
-
-  get champion(): Fighter {
-    return this._champion;
-  }
-
-  set champion(value: Fighter) {
-    this._champion = value;
-  }
 }
--- fighters-stats/src/app/fighter/fighter.model.ts
+++ fighters-stats/src/app/fighter/fighter.model.ts
@@ -1,16 +1,17 @@
 export class Fighter {
-  private _id: number;
-  private _firstName: string;
-  private _name: string;
-  private _age: number;
-  private _weight: number;
-  private _height: number;
-  private _reach: number;
-  private _nbWin: number;
-  private _nbLose: number;
+  _id: number;
+  _firstName: string;
+  _name: string;
+  _age: number;
+  _weight: number;
+  _height: number;
+  _reach: number;
+  _nbWin: number;
+  _nbLose: number;
+  _sexe: string;
 
 
-  constructor(id: number, firstName: string, name: string, age: number, weight: number, height: number, reach: number, nbWin: number, nbLose: number) {
+  constructor(id: number, firstName: string, name: string, age: number, weight: number, height: number, reach: number, nbWin: number, nbLose: number, sexe: string) {
     this._id = id;
     this._firstName = firstName;
     this._name = name;
@@ -20,78 +21,7 @@ export class Fighter {
     this._reach = reach;
     this._nbWin = nbWin;
     this._nbLose = nbLose;
+    this._sexe = sexe;
   }
 
-
-  get id(): number {
-    return this._id;
-  }
-
-  set id(value: number) {
-    this._id = value;
-  }
-
-  get firstName(): string {
-    return this._firstName;
-  }
-
-  set firstName(value: string) {
-    this._firstName = value;
-  }
-
-  get name(): string {
-    return this._name;
-  }
-
-  set name(value: string) {
-    this._name = value;
-  }
-
-  get age(): number {
-    return this._age;
-  }
-
-  set age(value: number) {
-    this._age = value;
-  }
-
-  get weight(): number {
-    return this._weight;
-  }
-
-  set weight(value: number) {
-    this._weight = value;
-  }
-
-  get height(): number {
-    return this._height;
-  }
-
-  set height(value: number) {
-    this._height = value;
-  }
-
-  get reach(): number {
-    return this._reach;
-  }
-
-  set reach(value: number) {
-    this._reach = value;
-  }
-
-  get nbWin(): number {
-    return this._nbWin;
-  }
-
-  set nbWin(value: number) {
-    this._nbWin = value;
-  }
-
-  get nbLose(): number {
-    return this._nbLose;
-  }
-
-  set nbLose(value: number) {
-    this._nbLose = value;
-  }
 }
