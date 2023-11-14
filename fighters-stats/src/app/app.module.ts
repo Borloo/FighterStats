7779e2d15e0516ac1ab4475373c4942e0b81da31
Borloo <supermaxime753@gmail.com>
2023-11-14 21:50:14 +0100

    Ok je crois qu'on est pas mal

--- fighters-stats/src/app/app.module.ts
+++ fighters-stats/src/app/app.module.ts
@@ -3,20 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FightersComponent } from './fighters/fighters.component';
 import { FighterComponent } from './fighter/fighter.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FightersComponent,
     FighterComponent,
-    CategoryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- fighters-stats/src/app/fighter/fighter.component.ts
+++ fighters-stats/src/app/fighter/fighter.component.ts
@@ -1,23 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {Fighter} from "./fighter.model";
-import {FighterService} from "./fighter.service";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-fighter',
   templateUrl: './fighter.component.html',
   styleUrls: ['./fighter.component.scss']
 })
-export class FighterComponent implements OnInit{
-
-  fightersList: Array<Fighter>|undefined
-
-  constructor(private fighterService: FighterService) {
-  }
-
-  ngOnInit() {
-    this.fighterService.getAllFighters().subscribe(resFighters => {
-      this.fightersList = resFighters
-    })
-  }
+export class FighterComponent {
 
 }
